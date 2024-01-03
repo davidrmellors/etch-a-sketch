149723298f380a6d50a1c1d918152b0c0f7df71c
David Mellors <davidroymellors@gmail.com>
2024-01-03 11:28:54 +0200

    Finish Etch-a-Sketch UI and Logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const containerDiv = document.querySelector('.container-div');
 const numOfSquaresBtn = document.querySelector('.number-of-squares-btn');
-let numberOfSquaresPerSide = 0;
+const clearBtn = document.querySelector('.clear-btn');
+let gridSize = 0;
 let validInput = false;
 
 let mouseDown = false;
@@ -8,32 +9,24 @@ document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
 numOfSquaresBtn.addEventListener('click', () => {
-
+    
     while(validInput == false){
-        numberOfSquaresPerSide = prompt('Please enter the number of squares for your new grid.');
-        if(numberOfSquaresPerSide >= 0 && numberOfSquaresPerSide <= 100)
+        gridSize = prompt('Please enter the number of squares for your new grid.');
+        if(gridSize >= 0 && gridSize <= 100)
         {validInput = true;} 
     }
     validInput = false;
 
-    let boxSize = containerDiv.clientWidth/numberOfSquaresPerSide;
-    
+    createGrid(gridSize);
+});
 
-    containerDiv.style.gridTemplateColumns = `repeat(${numberOfSquaresPerSide}, 1fr)`;
-    containerDiv.style.gridTemplateRows = `repeat(${numberOfSquaresPerSide}, 1fr)`;
-    for(let i = 0; i <= numberOfSquaresPerSide * numberOfSquaresPerSide; i++){
-
-        const gridElement = document.createElement('div');
-        gridElement.classList.add('grid-element');
-        gridElement.addEventListener('mouseover', paint);
-        gridElement.addEventListener('mousedown', paint);
-        containerDiv.appendChild(gridElement);
-            
-    }
+clearBtn.addEventListener('click', () => {
+    containerDiv.innerHTML = '';
+    createGrid(gridSize);
 });
 
 function paint(e) {
-    event.preventDefault();
+    e.preventDefault();
     if (e.type === 'mouseover' && !mouseDown){
         return;
     } 
@@ -42,6 +35,22 @@ function paint(e) {
     }
 }
 
+
+function createGrid(size){
+    let boxSize = containerDiv.clientWidth/size;
+    
+
+    containerDiv.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    containerDiv.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for(let i = 0; i <= size * size; i++){
+
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', paint);
+        gridElement.addEventListener('mousedown', paint);
+        containerDiv.appendChild(gridElement);       
+    }
+}
 
 
 
